694e16afaf11e0df0773c8c1470e3acf320fbed6
cuijiaxing <cuijiaxing@gmail.com>
2014-04-23 16:19:06 -0400

    added connect to server

--- proxy.c
+++ proxy.c
@@ -40,6 +40,40 @@ int main(int argc, char** argv)
 	}
 }
 
+//helper function that establishes a connection with a server
+//returns descriptor is OK, -1 on Unix error, -2 on DNS error
+int open_clientfd(char* hostname, int port){
+	int clientfd;
+	struct hostent * hp;
+	struct sockaddr_in serveraddr;
+
+	if((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		return -1;
+	}
+	if((hp = gethostbyname(hostname)) == NULL){
+		return -2;
+	}
+	bzero((char*) &serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	bcopy((char*)hp->h_addr_list[0],
+		(char*)&serveraddr.sin_addr.s_addr, hp->h_length);
+	serveraddr.sin_port = htons(port)
+
+	if(connect(clientfd, (SA*)&serveraddr, sizeof(serveraddr)) < 0){
+		return -1;
+	}
+	return clientfd;
+}
+
+void send_request_to_server(char* message, int port){
+	int fd = open_clientfd(message, port);
+	Rio_writen(fd, message, sizeof(message));
+	rio_t rio;
+	char buffer[MAXLINE];
+	Rio_readinitb(&rio, fd);
+	Rio_readlineb(&rio, buffer, MAXLINE);
+	printf("answer = %s\n", buffer);
+}
 
 void doit(int fd){
 	int is_static;
